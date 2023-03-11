ed63ba11d1e6a995607868044079f6066d7afe2d
MuhamedAbdelgalil <muhamed.abdelgalil@gmail.com>
2023-03-11 20:33:11 +0200

    adding spinner in search page

--- starter/src/pages/Search.js
+++ starter/src/pages/Search.js
@@ -1,28 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import * as BooksAPI from "../BooksAPI";
 import { Book } from "../components/Book";
 import { useDebounce } from "use-debounce";
+import Spinner from "../shared/components/Spinner";
 
 export const SearchPage = ({ books }) => {
+  const searchInputRef = useRef(null);
   const [query, setQuery] = useState("");
   const [value] = useDebounce(query, 500);
   const [sbooks, setSbooks] = useState([]);
   const [mapIdBooks, setMapIdBooks] = useState(new Map());
   const [comboBooks, setComboBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     BooksAPI.getAll().then((data) => {
       setMapIdBooks(createMapIdBooks(data));
+    }).finally(() => {
+      setIsLoading(false); searchInputRef.current.focus();
     });
     let isFoucse = true;
     if (value) {
       BooksAPI.search(value).then((data) => {
         if (data.error) {
           setSbooks([]);
+          setIsLoading(false);
         } else {
           if (isFoucse) {
             setSbooks(data);
+            setIsLoading(false);
           }
         }
       });
@@ -42,7 +50,7 @@ export const SearchPage = ({ books }) => {
       }
     });
     setComboBooks(combo);
-  }, [sbooks,mapIdBooks]);
+  }, [sbooks, mapIdBooks]);
 
   const createMapIdBooks = (books) => {
     const map = new Map();
@@ -51,6 +59,7 @@ export const SearchPage = ({ books }) => {
   };
 
   const upDateShelf = (book, shelf) => {
+    setIsLoading(true);
     const upDateBooks = sbooks.map((e) => {
       if (e.id === book.id) {
         book.shelf = shelf;
@@ -60,40 +69,50 @@ export const SearchPage = ({ books }) => {
     });
     setSbooks(upDateBooks);
     BooksAPI.update(book, shelf);
+    setIsLoading(false);
+
   };
 
   return (
-    <div className="search-books">
-      <div className="search-books-bar">
-        <Link to="/" className="close-search">
-          Close
-        </Link>
-        <div className="search-books-input-wrapper">
-          <input
-            type="text"
-            placeholder="Search by title, author, or ISBN"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          {console.log(sbooks)}
-        </div>
-      </div>
-      <div className="search-books-results">
-        {sbooks.length > 0 && (
-          <div>
-            <h3 style={{ textAlign: "center" }}>
-              Search returned {sbooks.length} books{" "}
-            </h3>
-            <ol className="books-grid">
-              {comboBooks.map((b) => (
-                <li key={b.id}>
-                  <Book book={b} books={books} upDateShelf={upDateShelf} />
-                </li>
-              ))}
-            </ol>
+    <>
+      {
+        isLoading ?
+          <Spinner /> :
+          <div className="search-books">
+            <div className="search-books-bar">
+              <Link to="/" className="close-search">
+                Close
+              </Link>
+              <div className="search-books-input-wrapper">
+                <input
+                  type="text"
+                  placeholder="Search by title, author, or ISBN"
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                  ref={searchInputRef}
+                />
+                {console.log(sbooks)}
+              </div>
+            </div>
+            <div className="search-books-results">
+              {sbooks.length > 0 ? (
+                <div>
+                  <h3 style={{ textAlign: "center" }}>
+                    Search returned {sbooks.length} books{" "}
+                  </h3>
+                  <ol className="books-grid">
+                    {comboBooks.map((b) => (
+                      <li key={b.id}>
+                        <Book book={b} books={books} upDateShelf={upDateShelf} />
+                      </li>
+                    ))}
+                  </ol>
+                </div>
+              ) : <span> No Available Books Matched!</span>}
+            </div>
           </div>
-        )}
-      </div>
-    </div>
+      }
+    </>
+
   );
 };
